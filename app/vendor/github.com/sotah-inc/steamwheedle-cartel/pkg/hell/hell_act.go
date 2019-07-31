2833302f5ecb248b7e0b4a8526b1d881f3113bbe
Adrian Parker <ihsw.aparker@gmail.com>
2019-07-30 17:25:20 -0700

    Fill in expected functionality.

---
+++ app/vendor/github.com/sotah-inc/steamwheedle-cartel/pkg/hell/hell_act.go
@@ -0,0 +1,29 @@
+package hell
+
+type ActEndpoints struct {
+	DownloadAuctions    string `firestore:"download_auctions"`
+	ComputeLiveAuctions string `firestore:"compute_live_auctions"`
+	Gateway             string `firestore:"gateway"`
+	CleanupManifests    string `firestore:"cleanup_manifests"`
+	CleanupAuctions     string `firestore:"cleanup_auctions"`
+	SyncItems           string `firestore:"sync_items"`
+}
+
+func (c Client) GetActEndpoints() (ActEndpoints, error) {
+	endpointsRef, err := c.FirmDocument("connection_info/act_endpoints")
+	if err != nil {
+		return ActEndpoints{}, err
+	}
+
+	docsnap, err := endpointsRef.Get(c.Context)
+	if err != nil {
+		return ActEndpoints{}, err
+	}
+
+	var actEndpoints ActEndpoints
+	if err := docsnap.DataTo(&actEndpoints); err != nil {
+		return ActEndpoints{}, err
+	}
+
+	return actEndpoints, nil
+}
